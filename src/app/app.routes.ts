ea641a544188699372f82783b46864fbff4e0121
matheusbail <matheus312henrique@gmail.com>
2024-05-28 16:08:40 -0300

    lot of changes, components, css, not final version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { LandPageComponent } from './land-page/land-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'', component: LandPageComponent}];
---
+++ src/app/land-page/land-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-land-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './land-page.component.html',
+  styleUrl: './land-page.component.css'
+})
+export class LandPageComponent {
+  
+}
