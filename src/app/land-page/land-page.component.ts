d00321f37715af2ca210c479b65751da1f45e956
matheusbail <matheus312henrique@gmail.com>
2024-05-29 15:38:23 -0300

    go horse

--- src/app/land-page/land-page.component.ts
+++ src/app/land-page/land-page.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WeatherPageComponent } from '../weather-page/weather-page.component';
 
 @Component({
   selector: 'app-land-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, WeatherPageComponent],
   templateUrl: './land-page.component.html',
   styleUrl: './land-page.component.css'
 })
 export class LandPageComponent {
+cityAddress="";
+
 showCardInit=true;
 showLogo=true;
 showSearch=true;
--- src/app/weather-page/weather-page.component.ts
+++ src/app/weather-page/weather-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { LandPageComponent } from '../land-page/land-page.component';
 
 @Component({
   selector: 'app-weather-page',
   standalone: true,
-  imports: [],
+  imports: [LandPageComponent],
   templateUrl: './weather-page.component.html',
   styleUrl: './weather-page.component.css'
 })
 export class WeatherPageComponent {
-
+  showWeek=true;
+  cityAddress: string;
+  constructor(){
+    this.cityAddress = "";
+  }
 }
