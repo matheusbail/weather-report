cb30d24b29d993d768b6a4112d4daa7eebbd46b5
matheusbail <matheus312henrique@gmail.com>
2024-06-06 16:27:53 -0300

    dbjson and more updates

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -12,10 +12,15 @@ export class WeatherService {
     cityInput = localStorage.getItem('Cidade');
     estateInput = "PR";
     countryInput = "BR";
-    
+    latInput=-25.3950986;
+    lonInput=-51.4622016;  
+ 
     getCityData(){
-      return this.http.get(`https://api.openweathermap.org/geo/1.0/direct?q=${this.cityInput},${this.estateInput},${this.countryInput}&limit=1&appid=1aec92b9eb5e7c3b0c5c89d5141406a2`)
-     
-    } 
+     return this.http.get(`https://api.openweathermap.org/geo/1.0/direct?q=${this.cityInput},${this.estateInput},${this.countryInput}&limit=1&appid=1aec92b9eb5e7c3b0c5c89d5141406a2`)
 
+    } 
+    getTemp(){
+      return this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${this.latInput}&lon=${this.lonInput}&appid=1aec92b9eb5e7c3b0c5c89d5141406a2`)
+    }
+    
 }
